d1a2ec8d4e911703c8e42ab26b652202665c7ab8
alpha-mind-coder <aditikapoor123aditi@gmail.com>
2025-07-06 21:17:51 +0530

    updated

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const multer = require('multer');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // 📂 Import Data
 const books = require('./data/books');
